44bed22dfdb64cb5f97ae6b2f97df78211f8bbaa
Sujal <112748674+SujalSharma-tech@users.noreply.github.com>
2025-04-30 13:00:46 +0530

    Updated backend url

--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -17,7 +17,7 @@ const RegisterPage = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://noti-fy-backend.onrender.com/api/v1/user/register",
+        "https://notessync-backend-281766668774.herokuapp.com/api/v1/user/register",
         { name, email, password },
         { withCredentials: true }
       );
